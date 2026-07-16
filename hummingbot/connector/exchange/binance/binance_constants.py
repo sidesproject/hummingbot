539b4d9cbb4b39a74f88b65158be2b33795af44e
Doug <dongdong@side.one>
2026-07-16 16:38:55 +0800

    fix bug

--- hummingbot/connector/exchange/binance/binance_constants.py
+++ hummingbot/connector/exchange/binance/binance_constants.py
@@ -114,8 +114,6 @@ RATE_LIMITS = [
                              LinkedLimitWeightPair(ORDERS, 1),
                              LinkedLimitWeightPair(ORDERS_24HR, 1),
                              LinkedLimitWeightPair(RAW_REQUESTS, 1)]),
-    RateLimit(limit_id="v1/listenKey", limit=MAX_REQUEST, time_interval=ONE_MINUTE,
-              linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 1)]),
 ]
 
 ORDER_NOT_EXIST_ERROR_CODE = -2013
